136b2b7a8cb413509d83a5f2ad04d4dcf3fe76e7
KHa321Z <khammadzaeem@gmail.com>
2025-06-13 12:26:08 +0500

    Single Step Debugger

--- Code/debug.asm
+++ Code/debug.asm
@@ -2,18 +2,87 @@
 
     jmp start
 
-orig_ss:    dw 0
 orig_sp:    dw 0
-oldkbisr:   dd 0
 oldtrapisr: dd 0
+oldkbisr:   dd 0
+oldretisr:  dd 0
 filepath:   times 128 db 0
 childseg:   dw 0
 
+flag:       db 0
+names:      db 'FL =CS =IP =BP =AX =BX =CX =DX =SI =DI =DS =ES ='
+
 kbisr:
-    ret
+    push ax
+
+    in al, 60h
+    test al, 80h
+    jnz skipflag
+    add byte [cs:flag], al
+
+skipflag:
+    mov al, 20h
+    out 20h, al
+
+    pop ax
+    iret
 
 trapisr:
-    ret
+    push bp
+    mov bp, sp
+
+    pusha
+    push ds
+    push es
+
+    sti
+    push cs
+    pop ds
+
+    mov byte [flag], 0
+    call clrscrn
+
+    mov si, 6
+    mov cx, 12
+    mov ax, 0
+    mov bx, 5
+
+l3:
+    push ax
+    push bx
+    mov dx, [bp + si]
+    push dx
+    call printnum
+    sub si, 2
+    inc ax
+    loop l3
+
+    mov ax, 0
+    mov bx, 0
+    mov cx, 12
+    mov si, 4
+    mov dx, names
+
+l1:
+    push ax
+    push bx
+    push dx
+    push si
+    call printstr
+    add dx, 4
+    inc ax
+    loop l1
+
+keywait:
+    cmp byte [flag], 0
+    je keywait
+
+    pop es
+    pop ds
+    popa
+
+    pop bp
+    iret
 
 hookISR: 
     push ax
@@ -26,16 +95,26 @@ hookISR:
     mov [oldtrapisr], ax
     mov ax, [es:1 * 4 + 2]
     mov [oldtrapisr + 2], ax
+    
     mov ax, [es:9 * 4]
     mov [oldkbisr], ax
     mov ax, [es:9 * 4 + 2]
     mov [oldkbisr + 2], ax
 
+    mov ax, [es:22 * 4]
+    mov [oldretisr], ax
+    mov ax, [es:22 * 4 + 2]
+    mov [oldretisr + 2], ax
+
     cli
     mov word [es:1 * 4], trapisr
     mov [es:1 * 4 + 2], cs
+
     mov word [es:9 * 4], kbisr
     mov [es:9 * 4 + 2], cs
+
+    mov word [es:22 * 4], return_to_parent
+    mov [es:22 * 4 + 2], cs
     sti
 
     pop es
@@ -55,10 +134,16 @@ unhookISR:
     mov [es:1 * 4], ax
     mov ax, [oldtrapisr + 2]
     mov [es:1 * 4 + 2], ax
+
     mov ax, [oldkbisr]
     mov [es:9 * 4], ax
     mov ax, [oldkbisr + 2]
     mov [es:9 * 4 + 2], ax
+    
+    mov ax, [oldretisr]
+    mov [es:22 * 4], ax
+    mov ax, [oldretisr + 2]
+    mov [es:22 * 4 + 2], ax
     sti
 
     pop es
@@ -68,7 +153,7 @@ unhookISR:
 
 start:
     ;-----hook interrupt service routines-----
-    ; call hookISR
+    call hookISR
 
     ;-----read file path-----
     xor cx, cx
@@ -100,8 +185,9 @@ start:
     mov [childseg], ax
 
     ;-----creating psp for child process-----
-    mov ah, 26h
+    mov ah, 55h
     mov dx, [childseg]
+    mov si, 0
     int 21h
 
     ;-----open .COM file-----
@@ -116,9 +202,8 @@ start:
     mov bx, ax
     mov ah, 3Fh
     mov cx, 0FFFFh
-    mov dx, [childseg]
-    mov ds, dx
     mov dx, 100h
+    mov ds, [childseg]
     int 21h
     jc file_not_read
 
@@ -128,25 +213,10 @@ start:
     mov ah, 3Eh
     int 21h
 
-    ;-----set up child process environment-----
-    mov ax, [childseg]
-    mov es, ax
-
-    ; setting return address for child process
-    mov word [es:0Ah], return_to_parent
-    mov word [es:0Ch], cs
-    mov word [es:0Eh], return_to_parent
-    mov word [es:10h], cs
-    mov word [es:12h], return_to_parent
-    mov word [es:14h], cs
-    ; setting parent psp
-    mov word [es:16h], cs
-
-    ; setting child process stack
-    mov [orig_ss], ss
+    ;-----set up child process stack-----
     mov [orig_sp], sp
 
-    mov ss, ax
+    mov ss, [childseg]
     mov sp, 0FFFEh
 
     pushf
@@ -160,12 +230,17 @@ start:
 
     ;-----come back to original process-----
 return_to_parent:
-    mov sp, [orig_sp]
-    mov ax, [orig_ss]
+    ; reset parent's registers
+    mov ax, cs
+    mov ds, ax
     mov ss, ax
+    mov sp, [orig_sp]
+
+    call unhookISR
 
     jmp terminate
 
+;-----error handling-----
 no_filepath:
     jmp no_filepath
 
@@ -177,6 +252,99 @@ no_memory_available:
 
 file_not_read:
     jmp file_not_read
+
+
+
+printstr: push bp
+mov bp, sp
+push es
+push ax
+push bx
+push cx
+push dx
+push si
+push di
+mov ax, 0xb800
+mov es, ax ; point es to video base
+mov di, 80 ; load di with columns per row
+mov ax, [bp+10] ; load ax with row number
+mul di ; multiply with columns per row
+mov di, ax ; save result in di
+add di, [bp+8] ; add column number
+shl di, 1 ; turn into byte count
+mov si, [bp+6] ; string to be printed
+mov cx, [bp+4] ; length of string
+mov ah, 0x07 ; normal attribute is fixed
+nextchar: mov al, [si] ; load next char of string
+mov [es:di], ax ; show next char on screen
+add di, 2 ; move to next screen location
+add si, 1 ; move to next char
+loop nextchar ; repeat the operation cx times
+pop di
+pop si
+pop dx
+pop cx
+pop bx
+pop ax
+pop es
+pop bp
+ret 8
+
+
+printnum: push bp
+mov bp, sp
+push es
+push ax
+push bx
+push cx
+push dx
+push di
+mov di, 80 ; load di with columns per row
+mov ax, [bp+8] ; load ax with row number
+mul di ; multiply with columns per row
+mov di, ax ; save result in di
+add di, [bp+6] ; add column number
+shl di, 1 ; turn into byte count
+add di, 8 ; to end of number location
+mov ax, 0xb800
+mov es, ax ; point es to video base
+mov ax, [bp+4] ; load number in ax
+mov bx, 16 ; use base 16 for division
+mov cx, 4 ; initialize count of digits
+nextdigit: mov dx, 0 ; zero upper half of dividend
+div bx ; divide by 10
+add dl, 0x30 ; convert digit into ascii value
+cmp dl, 0x39 ; is the digit an alphabet
+jbe skipalpha ; no, skip addition
+add dl, 7 ; yes, make in alphabet code
+skipalpha: mov dh, 0x07 ; attach normal attribute
+mov [es:di], dx ; print char on screen
+sub di, 2 ; to previous screen location
+loop nextdigit ; if no divide it again
+pop di
+pop dx
+pop cx
+pop bx
+pop ax
+pop es
+pop bp
+ret 6
+
+
+clrscrn: push es
+push ax
+push di
+mov ax, 0xb800
+mov es, ax ; point es to video base
+mov di, 0 ; point di to top left column
+nextloc: mov word [es:di], 0x0720 ; clear next char on screen
+add di, 2 ; move to next screen location
+cmp di, 4000 ; has the whole screen cleared
+jne nextloc ; if no clear next position
+pop di
+pop ax
+pop es
+ret
 
 
 
